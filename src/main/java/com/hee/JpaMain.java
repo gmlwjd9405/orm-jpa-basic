c4af05823ffd129f6274f3f6bb43d6918291eb02
kwonheejeong <gmlwjd9405@naver.com>
2019-08-04 20:29:00 +0900

    EntityManagerFactory, EntityManager 설명 주석 추가

--- src/main/java/com/hee/JpaMain.java
+++ src/main/java/com/hee/JpaMain.java
@@ -8,12 +8,15 @@ import javax.persistence.Persistence;
 public class JpaMain {
 
     public static void main(String[] args) {
-        // EntityManagerFactory: application loading 시점에 딱 하나만 생성되어야 함.
+        /* EntityManagerFactory
+         * application(WebServer) loading 시점에 DB 당 딱 하나만 생성되어야 함. */
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
-        // EntityManager: 실제 Transaction 단위를 수행할 때마다 생성
+        /* EntityManager
+         * 실제 Transaction 단위를 수행할 때마다 생성 (고객의 요청이 올 때마다 사용했다가 닫음)
+         * thread 간에 공유하면 안 됨. (사용하고 버려야 함) */
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        // Data 를 변경하는 모든 작업은 반드시 Transaction 안에서 이루어져야 함.
+        // Data 를 "변경"하는 모든 작업은 반드시 Transaction 안에서 이루어져야 함.
         EntityTransaction tx = entityManager.getTransaction();
         tx.begin();
 
@@ -22,6 +25,11 @@ public class JpaMain {
             Member findMember = entityManager.find(Member.class, 1L); // select
             System.out.println("findMember.id = " + findMember.getId());
             System.out.println("findMember.name = " + findMember.getName());
+
+            findMember.setName("changeName"); // update
+//            entityManager.persist(findMember); // update 시 불필요 (transaction commit 전에 변경사항을 모두 확인하여 update 하기 때문)
+
+//            entityManager.remove(findMember); // delete
 
             tx.commit();
         } catch (Exception e) {
