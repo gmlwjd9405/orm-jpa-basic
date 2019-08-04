18d7cf13a599d5f09892ecc7036ebd50c8b05228
kwonheejeong <gmlwjd9405@naver.com>
2019-08-04 20:12:57 +0900

    Member insert 예시를 통한 JPA 정석 코드

--- src/main/java/com/hee/JpaMain.java
+++ src/main/java/com/hee/JpaMain.java
@@ -1,7 +1,36 @@
 package com.hee;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class JpaMain {
+
     public static void main(String[] args) {
-        System.out.println("basic setting");
+        // EntityManagerFactory: application loading 시점에 딱 하나만 생성되어야 함.
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        // EntityManager: 실제 Transaction 단위를 수행할 때마다 생성
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        // Data 를 변경하는 모든 작업은 반드시 Transaction 안에서 이루어져야 함.
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        try {
+            /* code 작성 */
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+
+            entityManager.persist(member); // save
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            entityManager.close(); // 중요!
+        }
+        entityManagerFactory.close();
     }
 }
